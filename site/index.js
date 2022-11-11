513d8b7512eec7045f1cc04bbb456cb8bd3ae804
Ryohskay on Pop <learnerryo@gmail.com>
2022-11-11 22:03:50 +0000

    added comment

--- site/index.js
+++ site/index.js
@@ -1,3 +1,3 @@
-import("./node_modules/hello-wasm/hello_wasm.js").then((js) => {
+import("./node_modules/hello-wasm/hello_wasm.js").then((js) => {  // Not a good practice
   js.greet("WebAssembly with npm");
 });
