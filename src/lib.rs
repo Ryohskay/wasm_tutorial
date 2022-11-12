8f4fb71ea98e1d26bcb7cc1d124c146c2ebb8ba9
Ryohskay on Pop <learnerryo@gmail.com>
2022-11-12 13:37:19 +0000

    playing around

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+    pub fn querySelector(selectors: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+    querySelector("h1");  // yields querySelector not defined err
 }
